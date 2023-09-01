3ecec85fc9ef057c4f60b653e743c62552cdb1b9
Rafał Majewski <rmajewski@gabos.pl>
2023-09-01 20:32:24 +0200

    Dodanie settings z local i remote path

---
+++ GabosPacsUpdater/Model/UpdateService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GabosPacsUpdater.Model
+{
+    public class UpdateService
+    {
+        public void UpdatePacsDimse()
+        {
+
+        }
+
+        public void UpdatePacsWadoRS()
+        {
+
+        }
+
+        public void UpdatePacsHangfire()
+        {
+
+        }
+    }
+}
